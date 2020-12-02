333dc771c0728f07c18ed78ca435463f157c2aac
Lukaszwutkowski <64429535+Lukaszwutkowski@users.noreply.github.com>
2020-12-02 22:47:50 +0100

    small changes in html templates

---
+++ src/main/java/com/projectfinal/finalproject/config/SpringSecurityAuditorAware.java
@@ -0,0 +1,18 @@
+package com.projectfinal.finalproject.config;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String userName = authentication.getName();
+
+        return Optional.ofNullable(userName).filter(s -> !s.isEmpty());
+    }
+}
